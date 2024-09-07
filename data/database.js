2e5217c4f0dfe81f18b713af1821b61ca7d5991d
dev-luciana <luciana.freire.moura@outlook.com>
2024-09-07 18:09:07 -0300

    commit inicial

---
+++ data/database.js
@@ -0,0 +1,12 @@
+const banco = {
+    esportes: [
+        {
+            titulo: 'Surf',
+            descricao: 'Esporte aquatico'
+        },
+        {
+            titulo: 'Paraquedismo',
+            descricao: 'Esporte aerio'
+        }
+    ]
+}
